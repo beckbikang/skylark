71de023987c792b688be0f1386a468bf23df406c
bikang1 <bikang1@staff.weibo.com>
2021-10-04 22:08:14 +0800

    add code

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ skylark is an alerting service writing in golang
 
 ![架构图](http://ww1.sinaimg.cn/large/9b7f515dgy1gs31fetqfwj20hj0b9js0.jpg)
 
+## step
+
+1. start a project [done]
+2. use cobra make cmd [done]
+3. 
---
+++ internal/log/init.go
@@ -0,0 +1,27 @@
+package log
+
+import (
+	"github.com/beckbikang/flg"
+	"go.uber.org/zap"
+)
+
+//you can define you log var outer
+var (
+	Sl   *zap.Logger
+	gflg *flg.Logger
+)
+
+// init log
+func initLog(filePath string) {
+	gflg = &flg.Logger{}
+
+	err := gflg.LoadFromFile(filePath)
+	if err != nil {
+		panic("get file faild")
+	}
+
+	Sl, err = gflg.GetLogByKey("test")
+	if err != nil {
+		panic(err)
+	}
+}
